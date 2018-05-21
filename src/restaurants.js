2e435ad1261da44fef9d9053366f57460df77657
Leon <leeleonfly@gmail.com>
2018-05-21 16:13:42 -0700

    Add more Restaurant locations

--- src/restaurants.js
+++ src/restaurants.js
@@ -1,20 +1,20 @@
 export const restaurants = [
   {
-    name: "Shancheng Lameiz",
+    name: "MJ Cafe & Teahouse",
     location: {
-      lat: 33.9960177, lng: -117.8888179
-    }
-  },
-  {
-    name: "Garden Cafe",
-    location: {
-      lat: 33.9872942, lng: -117.90337482
+      lat: 33.991693, lng: -117.923140
     }
   },
   {
     name: "New Capital Seafood Restaurant",
     location: {
       lat: 33.9933116, lng: -117.9033421
+    }
+  },
+  {
+    name: "Garden Cafe",
+    location: {
+      lat: 33.9872942, lng: -117.90337482
     }
   },
   {
@@ -27,6 +27,30 @@ export const restaurants = [
     name: "Jersey Mike's Subs",
     location: {
       lat: 33.9948474, lng: -117.9070701
+    }
+  },
+  {
+    name: "Souplantation",
+    location: {
+      lat: 33.991235, lng: -117.926996
+    }
+  },
+  {
+    name: "El Pollo Loco",
+    location: {
+      lat: 33.992569, lng: -117.889126
+    }
+  },
+  {
+    name: "Pacific Plaza",
+    location: {
+      lat: 33.989059, lng: -117.902656
+    }
+  },
+  {
+    name: "Banana Bay",
+    location: {
+      lat: 33.988295, lng: -117.906514
     }
   }
 ]
